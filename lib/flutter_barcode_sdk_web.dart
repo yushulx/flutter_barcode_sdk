5c58e788f056f8e6e8ed1490d99e6d27967f1e88
yushulx <lingxiao1002@gmail.com>
2023-02-17 13:33:43 +0800

    Updated

--- lib/flutter_barcode_sdk_web.dart
+++ lib/flutter_barcode_sdk_web.dart
@@ -1,10 +1,5 @@
 import 'dart:async';
-// In order to *not* need this ignore, consider extracting the "web" version
-// of your plugin as a separate package, instead of inlining it in the same
-// package as the core of your plugin.
-// ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html show window;
-// import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_sdk/barcode_manager.dart';
