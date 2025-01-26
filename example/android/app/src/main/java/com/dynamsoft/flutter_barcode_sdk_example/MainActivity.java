d09adb8bbcb0fcc2689d12c3d76b2b76009510c2
yushulx <lingxiao1002@gmail.com>
2025-01-26 17:01:12 +0800

    Update for Android example

---
+++ example/android/app/src/main/java/com/dynamsoft/flutter_barcode_sdk_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.dynamsoft.flutter_barcode_sdk_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- example/android/app/src/main/kotlin/com/example/flutter_barcode_sdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_barcode_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
